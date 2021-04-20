15f255f97d7726647fcdc8d6fd8a1c0197f8b241
Henry Nicholson <dr.ex10011@gmail.com>
2021-04-20 16:25:42 +1200

    Making some changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from renderer import Display
 from keyboard import Keyboard
 from speaker import Speaker
@@ -8,10 +9,31 @@ def main():
 	keyboard = Keyboard()
 	speaker = Speaker()
 	renderer = Display(10, keyboard, speaker)
+	cpu = CPU(renderer, keyboard, speaker)
+
+	# First argument should be the rom path
+	rom_path = sys.argv[1]
+
+	# Load the rom into memory
+	CPU
+
+	then = time.perf_counter()
 
 	fps = 60
 	fpsInterval = 1000 // fps
 	startTime = then
+	elapsed = 0
+
+	running = True
+	while running:
+		now = time.perf_counter()
+		elapsed = now - then
+
+		if (elapsed > fpsInterval):
+			CPU.cycle()
+
+
+
 
 if __name__ == "__main__":
 	main()
